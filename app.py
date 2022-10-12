85bc6577cf2f6eed9acf64a7125a47e929ea8912
paytonshen <payton_shen@mynbps.org>
2022-10-11 23:15:30 -0400

    Finished project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -8,13 +8,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    subtile = db.Column(db.String(255), nullable=False)
+    subtitle = db.Column(db.String(255), nullable=False)
     body = db.Column(db.Text, nullable=False)
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'),nullable=False)
@@ -23,7 +20,6 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post %r>' % self.title
 
-
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -31,14 +27,57 @@ class Category(db.Model):
     def __repr__(self):
         return '<Category %r>' % self.name
 
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    category_name = request.form["category"]
+    category = Category(name=category_name)
+    post = Post(
+        title=request.form["title"],
+        subtitle=request.form["subtitle"],
+        body=request.form["body"],
+        category=category
+    )
+    post_to_delete = post.query.get_or_404(post_id)
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+    except:
+        return "rip"
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template("index.html")
+def get_all_posts():  # put application's code here
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
 
 
-@app.route('/create-post')
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    fetched_post = Post.query.get(post_id)
+    return render_template("post.html", post=fetched_post)
+
+
+
+
+@app.route('/create-post', methods=['GET', 'POST'])
 def create_post():
-    return render_template("create-post.html")
+    if request.method == "GET":
+        return render_template("create-post.html")
+    else:
+        category_name = request.form["category"]
+        category = Category(name=category_name)
+        post = Post(
+            title=request.form["title"],
+            subtitle=request.form["subtitle"],
+            body=request.form["body"],
+            category=category
+        )
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+
+
 
 
 if __name__ == '__main__':
